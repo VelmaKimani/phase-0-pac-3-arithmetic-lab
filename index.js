8e5d7f0ac13da488db2e265763a58a2f6122f994
Velma Kimani <stanvelma16@gmail.com>
2023-09-13 09:44:03 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+//reset number
+number = 10;
+
+function add(a, b) {
+    return a + b;
+  //return (number += 5);
+}
+console.log(add(5, 5));
+
+function subtract(a, b) {
+    return a - b;
+   //return (number -= 5);
+  }
+  console.log(subtract(10, 5));
+ 
+  function multiply(a, b) {
+    return a * b;
+   //return (number *= 5);
+  }
+  console.log(multiply(5, 5));
+
+function divide(a, b) {
+    return a / b;
+  //return (number /= 3);
+}
+console.log(divide(40, 8));
+  
+n = 5;
+function increment(n){
+    
+    return n + 1;
+}
+console.log(n);
+
+function decrement(n){
+     
+     return n - 1;
+}
+console.log(n);
+
+function makeInt(n){
+    parseInt("n", 10);
+    return n;
+}
+makeInt('0*2328');
+console.log(makeInt(5));
+
+function preserveDecimal(n){
+    parseFloat('2.222');
+    return n;
+}
+console.log(preserveDecimal(n));
